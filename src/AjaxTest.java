e58b852c79ccce01e4a8b98ee0af807fdfc219a5
Yicong-Huang <hyc541978023@gmail.com>
2018-05-17 00:27:03 -0700

    formatted

--- src/AjaxTest.java
+++ src/AjaxTest.java
@@ -1,28 +1,29 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Calendar;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
 @WebServlet("/servlet/AjaxTest")
 public class AjaxTest extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public AjaxTest() { super(); }
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Calendar c = Calendar.getInstance ();
+    public AjaxTest() {
+        super();
+    }
 
-		response.setContentType("text/html");
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Calendar c = Calendar.getInstance();
 
-	    PrintWriter out = response.getWriter();
-	    out.println(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
-	}
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+    }
 }
